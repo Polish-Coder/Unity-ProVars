e531f8958128cd5c9b6840f292fc715ddeee0a2b
Polish Coder <84400281+Polish-Coder@users.noreply.github.com>
2023-03-27 21:54:29 +0200

    Added `DisplayUnit` attribute

---
+++ Attributes/DisplayUnitAttribute.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class DisplayUnitAttribute : PropertyAttribute
+{
+    public readonly string Unit;
+
+    public DisplayUnitAttribute(string unit)
+    {
+        Unit = unit;
+    }
+}
---
+++ Editor/DisplayUnitDrawer.cs
@@ -0,0 +1,22 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+[CustomPropertyDrawer(typeof(DisplayUnitAttribute))]
+public class DisplayUnitDrawer : PropertyDrawer
+{
+    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+    {
+        var displayUnit = attribute as DisplayUnitAttribute;
+
+        EditorGUI.BeginProperty(position, label, property);
+        position = EditorGUI.PrefixLabel(position, GUIUtility.GetControlID(FocusType.Passive), label);
+
+        Rect valueRect = new Rect(position.position, new Vector2(position.width - 60f, position.height));
+        Rect unitRect = new Rect(position.position + new Vector2(position.width - 50f, 0f), new Vector2(50f, position.height));
+
+        EditorGUI.PropertyField(valueRect, property, GUIContent.none);
+        EditorGUI.LabelField(unitRect, displayUnit?.Unit);
+
+        EditorGUI.EndProperty();
+    }
+}
